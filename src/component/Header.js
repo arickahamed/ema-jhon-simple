8d7b7d34deef584eb241873488608fb031026371
arickahamed <arickahamed700@gmail.com>
2020-07-13 16:53:13 +0600

    stuck in header logo

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,34 +1,12 @@
 import React from "react";
 import "./Header.css";
-import { useState } from "react";
-import { useRef } from "react";
-import { useEffect } from "react";
 import { useAuth } from "./LogIn/UseAuth";
-
-const usePrevious = (value) => {
-  const prev = useRef();
-  // useEffect(() => {
-  //   console.log(value);
-  //   prev.current = value;
-  // });
-  return prev.current;
-};
 
 const Header = () => {
   const auth = useAuth();
-  // useEffect(() => {
-  //   console.log(auth);
-  // }, []);
-
-  const [count, setCount] = useState(0);
-  const previous = usePrevious(count);
   return (
     <div className="header">
-      <h1>
-        Count:{count} Previous:{previous}
-      </h1>
-      <button onClick={() => setCount(count + 1)}>+</button>
-      <button onClick={() => setCount(count - 1)}>-</button>
+      <img src="../images/logo.png" alt="" />
       <nav>
         <a href="/shop">Shop</a>
         <a href="/review">Review</a>
